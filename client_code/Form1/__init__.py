c0034491c45df865ef232c1821e7bbe1e774e15d
nguyenduybach12a11@gmail.com <nguyenduybach12a11@gmail.com>
2024-05-03 03:33:02 +0000

    Created ServerModule1 and LICENSE.txt; Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,6 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.server
 
 
 class Form1(Form1Template):
@@ -8,3 +9,32 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def drop_down_sort_change(self, **event_args):
+    """This method is called when an item is selected"""
+    pass
+
+  def drop_down_sort_show(self, **event_args):
+    sorting_algorithms = ['Insertion Sort', 'Selection Sort', 'Bubble Sort', 'Merge Sort']
+    self.drop_down_sort.items = sorting_algorithms
+    pass
+
+  def button_sort_click(self, **event_args):
+    if(self.text_box_input_sort.text == ""):
+      alert("Yêu cầu nhập giá trị đầu vào")
+      return
+    step = []
+    lstNumber = anvil.server.call('convertStringToArr',self.text_box_input_sort.text)
+    selectSort = self.drop_down_sort.selected_value
+    if(selectSort == 'Insertion Sort'):
+      lstNumber, step = anvil.server.call('insertionSort',lstNumber)
+    elif(selectSort == 'Selection Sort'):
+      lstNumber, step = anvil.server.call('selectionSort',lstNumber, len(lstNumber))
+    elif(selectSort == 'Bubble Sort'):
+      lstNumber, step = anvil.server.call('bubbleSort',lstNumber)
+    else:
+      lstNumber = anvil.server.call('mergeSortResult',lstNumber)
+    a_string = ','.join(map(str, lstNumber))
+    self.label_result.text = "Kết quả : " + a_string
+    self.label_step.text = step
+    pass
---
+++ server_code/ServerModule1.py
@@ -0,0 +1,109 @@
+import anvil.server
+
+# This is a server module. It runs on the Anvil server,
+# rather than in the user's browser.
+#
+# To allow anvil.server.call() to call functions here, we mark
+# them with @anvil.server.callable.
+# Here is an example - you can replace it with your own:
+#
+# @anvil.server.callable
+# def say_hello(name):
+#   print("Hello, " + name + "!")
+#   return 42
+#
+@anvil.server.callable
+def convertStringToArr(str):
+  my_list = str.split(",")
+  my_list = [int(x) for x in my_list]
+  return my_list
+
+@anvil.server.callable
+def insertionSort(arr):
+    step = []
+    n = len(arr)  # Get the length of the array
+    if n <= 1:
+        return
+    for i in range(1, n):
+        key = arr[i]
+        j = i-1
+        while j >= 0 and key < arr[j]:
+            arr[j+1] = arr[j]
+            j -= 1
+        arr[j+1] = key
+        step.append(arr.copy())
+    return arr, step
+
+@anvil.server.callable
+def selectionSort(array, size):
+    step = []
+    for ind in range(size):
+        min_index = ind
+        for j in range(ind + 1, size):
+            if array[j] < array[min_index]:
+                min_index = j
+        (array[ind], array[min_index]) = (array[min_index], array[ind])
+        step.append(array.copy())
+    return array, step
+
+@anvil.server.callable
+def bubbleSort(arr):
+    n = len(arr)
+    step = []
+    for i in range(n-1):
+        swapped = False
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j + 1]:
+                swapped = True
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+        step.append(arr.copy())
+        if not swapped:
+            return
+    return arr, step
+
+def merge(arr, l, m, r):
+    n1 = m - l + 1
+    n2 = r - m
+ 
+    L = [0] * (n1)
+    R = [0] * (n2)
+
+    for i in range(0, n1):
+        L[i] = arr[l + i]
+ 
+    for j in range(0, n2):
+        R[j] = arr[m + 1 + j]
+ 
+    i = 0     
+    j = 0    
+    k = l     
+ 
+    while i < n1 and j < n2:
+        if L[i] <= R[j]:
+            arr[k] = L[i]
+            i += 1
+        else:
+            arr[k] = R[j]
+            j += 1
+        k += 1
+    while i < n1:
+        arr[k] = L[i]
+        i += 1
+        k += 1
+    while j < n2:
+        arr[k] = R[j]
+        j += 1
+        k += 1
+ 
+@anvil.server.callable
+def mergeSort(arr, l, r):
+    if l < r:
+        m = l+(r-l)//2
+        mergeSort(arr, l, m)
+        mergeSort(arr, m+1, r)
+        merge(arr, l, m, r)
+
+@anvil.server.callable
+def mergeSortResult(arr):
+  mergeSort(arr,0,len(arr) - 1)
+  return arr
